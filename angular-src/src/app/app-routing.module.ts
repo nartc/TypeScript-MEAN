e9223e7c70f46938096e2fa85092ab0e66a84947
Chau <ctch5@mail.umsl.edu>
2018-01-02 22:01:22 -0600

    Add routing

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {DefaultComponent} from './layouts/default/default.component';
+import {AuthComponent} from './layouts/auth/auth.component';
+import {TasksComponent} from './components/tasks/tasks.component';
+import {TaskAddComponent} from './components/tasks/task-add/task-add.component';
+import {TaskDetailComponent} from './components/tasks/task-detail/task-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: DefaultComponent,
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
+            }
+        ]
+    },
+    {
+        path: 'user/:id',
+        component: AuthComponent,
+        children: [
+            {
+                path: 'tasks',
+                component: TasksComponent,
+                children: [
+                    {
+                        path: 'add',
+                        component: TaskAddComponent
+                    },
+                    {
+                        path: ':taskId',
+                        component: TaskDetailComponent
+                    }
+                ]
+            }
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
